80e36a4ec00ff888cb1a2db4d8fa36ee85cc571a
Sudhan <86077071+Sudhan-Dahake@users.noreply.github.com>
2025-04-06 02:31:59 -0400

    Implemented a global console signal handler to make sure server shuts down gracefully when closed using Ctrl+C or 'X' icon in the terminal.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 #include "TCPServer.h"
 #include <thread>
+#include <Windows.h>
 
 #define PORT 27000
 
+// Global pointer to server instances.
+TCPServer* globalServer = nullptr;
+
+// This is a console signal handler.
+// Handles cases where the terminal is closed forcefully by pressing Ctrl+C or by clicking 'X' icon.
+BOOL WINAPI ConsoleHandler(DWORD signal) {
+	if ((signal == CTRL_C_EVENT) || (signal == CTRL_CLOSE_EVENT)
+		|| (signal == CTRL_BREAK_EVENT) || (signal == CTRL_LOGOFF_EVENT) || (signal == CTRL_SHUTDOWN_EVENT)) {
+		std::cout << "\n[!] Shutdown signal received. Cleaning up...\n";
+
+		if (globalServer) {
+			delete globalServer;
+
+			globalServer = nullptr;
+		};
+
+		ExitProcess(0);
+	};
+
+	return TRUE;
+};
+
+
 int main()
 {
-	/*UDPServer UDPServer(27000, 4);
-	UDPServer UDPServer(27000, 14);
+	// Registering the signal handler.
+	if (!SetConsoleCtrlHandler(ConsoleHandler, TRUE)) {
+		std::cout << "Failed to set control handler.\n";
 
-	UDPServer.Start();*/
+		return -1;
+	}
+
 	int num_threads = std::thread::hardware_concurrency(); //sets number of threads based on hardware
 
-	TCPServer TCPServer(PORT, num_threads);
+	globalServer = new TCPServer(PORT, num_threads);
 
-	TCPServer.Start();
+	globalServer->Start();
 
-	return 1;
+
+	delete globalServer;
+
+	globalServer = nullptr;
+
+	return 0;
 }
